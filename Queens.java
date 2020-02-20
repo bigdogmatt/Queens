06f8a1754cda2151cec2f94bbf7f44f381631570
Matt Kleman <matt.kleman02@gmail.com>
2020-02-20 17:41:53 -0500

    Cleanup

--- Queens.java
+++ Queens.java
@@ -1,6 +1,7 @@
 public class Queens 
 {
 	
+	//A method that prints the chess board to the console including the queens and thier locations
 	public static void printBoard(char[][] board) 
 	{
 		for(int i = 0; i < board.length; i++)
@@ -25,9 +26,10 @@ public class Queens
 		System.out.print("-");
 	}
 	
+	// A method that takes the locations of two queens and returns true if they can attack eachother and false if not
 	public static boolean canAttack(int row1, int column1, int row2, int column2 ) 
 	{
-		
+		// Two queens can attack eachother if they are in the same column, row, or diagonal
 		if(row1 == row2 || column1 == column2 || Math.abs(row1 - row2) == Math.abs(column1 - column2))
 		{
 			return true;
@@ -41,22 +43,26 @@ public class Queens
 	
 	
 	public static void main(String[] args) {
-		char[][] board = new char[8][8];
+		char[][] board = new char[8][8]; //create a new board
 		
+		// Randomly determine the row and column of the first queen
 		int row1 = (int)(Math.random() * 8);
 		int column1 = (int)(Math.random() * 8);	
 		int row2;
 		int column2;
 		
+		// Randomly determine the row and column of the second queen as long as they are not the same as the first
 		do {
 			row2 = (int)(Math.random() * 8);
 			column2 = (int)(Math.random() * 8);
 		} while( row2 == row1 && column2 == column1 );
 		
+		// Fill the board with empty spaces
 		for( int i = 0; i < board.length; ++i )
 			for( int j = 0; j < board[i].length; ++j )
 				board[i][j] = ' ';
 		
+		// Add the queens
 		board[row1][column1] = 'Q';
 		board[row2][column2] = 'Q';
 		
